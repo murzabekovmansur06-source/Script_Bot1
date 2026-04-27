b176b6402056c4261a98a2681539023306f43a5c
murzabekovmansur06-source <murzabekovmansur06@gmail.com>
2026-04-27 18:04:13 +0300

    bot.py

---
+++ bot.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+import os
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Бот {bot.user} запущен!')
+
+bot.run(os.environ['DISCORD_TOKEN'])
